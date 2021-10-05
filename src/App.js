260a844d364d9a837cf01fd001423f680b8f3e74
Laura Carazo <laura.carazo@mimbus.com>
2021-10-05 16:53:55 +0200

    transformation avatar en hook générique

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import {Form, Button,  Row, Col, Input, Layout} from "antd";
+import "antd/dist/antd.css";
+import AvatarOrg from './AvatarOrg';
+//import { ChromePicker } from 'react-color';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const onFinish = (values) => {
+		console.log('Success:', values);		
+	};
+
+	const onFinishFailed = (errorInfo) => {
+		console.log('Failed:', errorInfo);
+	};
+
+		
+	return (
+		<>
+		<Layout 
+				 
+		 >
+			<Row 	 justify="center">
+				<Col  span={12}>
+					<h2>Organisations / Création d'un centre</h2>
+				</Col>
+			</Row>   
+	 
+			<Row >   
+				<Col justify="left"  span={12} offset={4} >
+					<Form
+						name="organization-form"
+						labelCol={{
+						span: 8,
+						}}
+						wrapperCol={{
+						span: 20,
+						}}
+						onFinish={onFinish}
+						onFinishFailed={onFinishFailed}
+						autoComplete="off"
+					>
+						{/* Champ nom de l'organization */}
+						<Form.Item
+						
+								label="Nom de l'organisation"
+								name="orgName"
+								rules={[
+									{
+										required: true,
+										message: 'Ce champ est requis',
+									},
+								]}
+						>
+						<Input />										
+						</Form.Item>
+						
+						{/* Champ de text Notes */}
+						<Form.Item 
+								name="notes"
+								label="Notes">
+								<Input.TextArea />
+						</Form.Item>
+
+						{/* Champ logo */}
+						<Form.Item
+						wrapperCol={{
+							offset: 8,
+							span: 14,
+						}}
+						
+						name="avatar" >
+							<div >			
+							<AvatarOrg />
+							</div>
+						</Form.Item>
+						
+
+						<Form.Item
+							wrapperCol={{
+								offset: 8,
+								span: 16,
+							}}
+						>
+							<Button type="primary" htmlType="submit">
+								Submit
+							</Button>
+						</Form.Item>
+					</Form>
+				</Col>
+			</Row>
+		</Layout>
+		</>
+	);
 }
 
 export default App;
